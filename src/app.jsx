0857d688c5d3e554985a36d372f935a0e6fb1134
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-08-18 15:45:04 +0700

    feat: fetch api

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,12 @@
+import { use } from "react";
+
+import { fetchApi } from "#/lib/api.js";
+
+// oxlint-disable-next-line unicorn/prefer-top-level-await promise/prefer-await-to-then
+const fetchHello = fetchApi("/").then(async (res) => res.text());
+
 export function App() {
-	return <h1>Hello, World!</h1>;
+	const hello = use(fetchHello);
+
+	return <h1>{hello}</h1>;
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 
-import { App } from "#/app";
+import { App } from "#/app.jsx";
 
 const node = /** @type {!HTMLElement} */ (document.querySelector("#root"));
 const root = createRoot(node);
