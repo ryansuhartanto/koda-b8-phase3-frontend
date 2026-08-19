0df58b86038adfddd41911de0c828bca4e4d86df
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-08-19 10:32:26 +0700

    feat: styles

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,9 @@ import { use } from "react";
 
 import { fetchApi } from "#/lib/api.js";
 
+// oxlint-disable-next-line import/no-unassigned-import
+import "#/style.css";
+
 // oxlint-disable-next-line unicorn/prefer-top-level-await promise/prefer-await-to-then
 const fetchHello = fetchApi("/").then(async (res) => res.text());
 
